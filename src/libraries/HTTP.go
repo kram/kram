be84c83419f706f71322f31271c221b83b3a17bd
Gustav Westling <gustav@smsgrupp.se>
2015-06-08 15:11:27 +0200

    #50 - Continued work on a HTTP library. Added an example of how to use it

--- src/libraries/HTTP.go
+++ src/libraries/HTTP.go
@@ -6,34 +6,79 @@ package libraries
 
 import (
 	"github.com/zegl/Gus/src/types"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type Library_HTTP struct {
 	Library
+
+	request *http.Request
+	verb    string
+	uri     string
+	body    io.Reader
+	headers map[string]string
 }
 
 func (self Library_HTTP) Instance() (types.Lib, string) { return &Library_HTTP{}, self.Type() }
 func (self Library_HTTP) Type() string                  { return "HTTP" }
 func (self Library_HTTP) M_Type() *types.Class          { return self.String(self.Type()) }
 
-func (self Library_HTTP) M_Request(params []*types.Class) *types.Class {
-	if len(params) != 2 {
-		log.Panic("HTTP::Request() expects exactly 2 parameters")
-	}
+func (self *Library_HTTP) M_Request(params []*types.Class) *types.Class {
 
-	// Gus parameters
-	verb := params[0].ToString()
-	uri := params[1].ToString()
+	// Default parameters
+	verb := "GET"
+	uri := "http://localhost"
+
+	// Parse params
+	self.Params(params, &verb, &uri)
+
+	self.verb = verb
+	self.uri = uri
+	self.headers = make(map[string]string)
+	self.body = strings.NewReader("")
+
+	return self.Null()
+}
+
+func (self *Library_HTTP) M_Header(params []*types.Class) *types.Class {
+	key := ""
+	value := ""
+
+	self.Params(params, &key, &value)
+
+	self.headers[key] = value
+
+	return self.Null()
+}
+
+func (self *Library_HTTP) M_Body(params []*types.Class) *types.Class {
+	body := ""
+
+	self.Params(params, &body)
+
+	self.body = strings.NewReader(body)
+
+	return self.Null()
+}
+
+func (self *Library_HTTP) M_Do(params []*types.Class) *types.Class {
 
 	// Init Go net/http library
-	client := &http.Client{}
-	req, err := http.NewRequest(verb, uri, nil)
+	req, err := http.NewRequest(self.verb, self.uri, self.body)
 
 	self.Error(err)
 
+	req.Header.Set("User-Agent", "Gus/HTTP Library 0.1")
+
+	for key, val := range self.headers {
+		req.Header.Set(key, val)
+	}
+
+	client := &http.Client{}
 	resp, err := client.Do(req)
 
 	self.Error(err)
