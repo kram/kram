355663ee477621204e798a33ff870a2aedcf1d2b
Gustav Westling <hej@gustav.tv>
2015-11-30 22:54:03 +0100

    Removed unused include

--- src/vm/libraries/IO/io.h
+++ src/vm/libraries/IO/io.h
@@ -2,7 +2,6 @@
 // See the LICENSE file at the top-level directory of this distribution.
 // This file may not be copied, modified, or distributed except according to those terms.
 
-// #include <iostream>
 #include "../../output.h"
 #include "../../value.h"
 
